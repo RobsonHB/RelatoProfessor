cab4da1aae55397593266a3e7515ed57183405d5
Robson Herminio Belinho <robsonhb@uninove.br>
2020-10-29 11:48:14 -0300

    ajuste na tela de orientação (orientacao.php)

--- orientacao.php
+++ orientacao.php
@@ -1,73 +1,69 @@
 <!DOCTYPE html>
-<html lang="pt_br" >
+<html lang="pt_br">
 
 <head>
-	<title>HELP Professor- UNINOVE</title>
-	<meta charset="UTF-8">
-	<script src="js/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery-1.10.2.js"></script>
-	<script src="js/js.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/open-iconic.min.css">
-	<link rel="stylesheet" href="style1.css">
+  <title>HELP Professor - UNINOVE</title>
+  <meta charset="UTF-8">
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery-1.10.2.js"></script>
+  <script src="js/js.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/open-iconic.min.css">
 </head>
 
 <body>
-</head>
-<body>
     
-    
-    <center><figure>
-  <img src="media/PRO_HELP.png" alt="logo_uninove">	
-  <h1>Orientações</h1> 
-        </figure>
-    </center>
+  <center><img src="media/PRO_HELP.png" alt="logo_uninove"><h1>Orientações</h1></center><br>
+  <center>
+    <button class="btn btn-dark" id="videos">Videos</button>
+    <button class="btn btn-dark" id="documentacao">Documentos</button>
+  </center><br>
 
-  </div>
-  <a class='sub-title' href='https://uninove.br' target='_blank'> 
-  </a>
-</div>
- 
-      <center><h1>Conecte-se à rede Wi-Fi da UNINOVE</h1></center>
+  <!--<div>
+    <a class='sub-title' href='https://uninove.br' target='_blank'></a>
+  </div> -->
+
+  <div >
+   <div class="jumbotron"style="background-color: #1E364C">
+      <center><h4 style="color: white; ">Conecte-se à rede Wi-Fi da UNINOVE</h4></center>
+
       <center><video autoplay muted width="520" height="360" controls>
-   <source src="media/WifiProfessor.mp4" type="video/mp4">
-   <param name="allowFullScreen" value="true"></param>
-  <param name="allowscriptaccess" value="always"></param>
-  <embed src="media/WifiProfessor.mp4"
-    type="application/x-shockwave-flash"
-    allowscriptaccess="always"
-    allowfullscreen="true"
-        </video></center>
+          <source src="media/WifiProfessor.mp4" type="video/mp4">
+          <!-- <param name="allowFullScreen" value="true"></param>
+          <param name="allowscriptaccess" value="always"></param> -->
+          <embed src="media/WifiProfessor.mp4" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true">
+      </video></center>  
     </div>
-	
-	
-	<center><h1>Projetar utilizando smartphone ou tablet</h1></center>
-       <center><video autoplay muted width="520" height="360" controls>
-   <source src="media/projetor3.mov" type="video/mp4">
-   <param name="allowFullScreen" value="true"></param>
-  <param name="allowscriptaccess" value="always"></param>
-  <embed src="media/projetor3.mov"
-    type="application/x-shockwave-flash"
-    allowscriptaccess="always"
-    allowfullscreen="true"
-        </video></center>
+</div>
+    <div class="container">
+      <div>
+        <center><h4 style="color: #1E364C";">Projetar utilizando smartphone ou tablet</h4></center>
+        <center><video autoplay muted width="520" height="360" controls>
+          <source src="media/projetor3.mov" type="video/mp4">
+          <embed src="media/projetor3.mov" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true">
+        </video></center><br>
+      </div>
     </div>
-		<center><h1>Projetar utilizando Notebook</h1></center>
-       <center><video autoplay muted width="520" height="360" controls>
-   <source src="media/projetor2.mov" type="video/mp4">
-   <param name="allowFullScreen" value="true"></param>
-  <param name="allowscriptaccess" value="always"></param>
-  <embed src="media/projetor2.mov"
-    type="application/x-shockwave-flash"
-    allowscriptaccess="always"
-    allowfullscreen="true"
-        </video></center>
+
+    <div >
+    <div class="jumbotron" style="background-color: #1E364C">
+      <center><h4 style="color: white; ">Projetar utilizando Notebook</h4></center>
+      <center><video autoplay muted width="520" height="360" controls>
+        <source src="media/projetor2.mov" type="video/mp4">
+        <embed src="media/projetor2.mov" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true">
+      </video></center>
     </div>
-	 <hr>
-	
-	
-	
-	
-	
+  </div>
+
+  
+</body>
+</html>
+
+  
+
+
+    
+  
+  
     
